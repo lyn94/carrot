bae56a8dabe9e7e5953d0f6d0e68cbbae8391406
lyn94 <lyn_94@naver.com>
2022-07-06 23:20:39 +0900

    ~#9.2

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -3,7 +3,59 @@ import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/server/client";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { phone, email } = req.body;
+  const payload = phone ? {phone : +phone} : {email}
+  const user = await client.user.upsert({
+    where:{
+      ...payload
+
+    },
+    create: {
+      name: "Anonymous",
+      ...payload
+    },
+    update: {}
+  })
+  console.log(user);
+    
+  // if(email) {
+  //   user = await client.user.findUnique({
+  //     where: {
+  //       email,
+  //     }
+  //   });
+  //   if(user) console.log("찾았다")
+  //   if(!user) {
+  //     console.log("새로 만듬");
+  //     user = await client.user.create({
+  //       data: {
+  //         name:"Anonymous",
+  //         email,
+  //       }
+  //     })
+  //   }
+  //   console.log(user);
+    
+  // }
+  // if(phone) {
+  //   user = await client.user.findUnique({
+  //     where: {
+  //       phone: +phone,
+  //     }
+  //   });
+  //   if(user) console.log("찾았다")
+  //   if(!user) {
+  //     console.log("새로 만듬");
+  //     user = await client.user.create({
+  //       data: {
+  //         name:"Anonymous",
+  //         phone: +phone,
+  //       }
+  //     })
+  //   }
+  //   console.log(user);
+    
+  // }
   return res.status(200).end();
 }
 

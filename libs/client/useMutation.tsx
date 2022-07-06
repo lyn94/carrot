29baccd39af8dac00f0bd0ae0e0f22ae422e518a
lyn94 <lyn_94@naver.com>
2022-07-06 13:54:27 +0900

    ~#8.5

--- libs/client/useMutation.tsx
+++ libs/client/useMutation.tsx
@@ -1,28 +1,33 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export default function useMutation(url:string):[(data:any)=> void, {loading: boolean;data:undefined|any;error:undefined|any}] {
-    const [state, setState] = useState({
-        loading: false,
-        data:undefined,
-        error:undefined
+interface UseMutationState {
+  loading: boolean;
+  data?: object;
+  error?: object;
+}
+
+type UseMutationResult = [(data: any) => void, UseMutationState];
+
+export default function useMutation(url: string): UseMutationResult {
+  const [state, setState] = useState({
+    loading: false,
+    data: undefined,
+    error: undefined,
+  });
+
+  function mutation(data?: any) {
+    setState((prev) => ({ ...prev, loading: true }));
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
     })
-    const [loading, setLoading] = useState(false)
-    const [data, setData] = useState<undefined|any>(undefined);
-    const [error, setError] = useState<undefined|any>(undefined);
-
-    function mutation(data?:any){
-        setLoading(true);
-        fetch(url, {
-            method:"POST",
-            headers: {
-                "Content-Type":"application/json"
-            },
-            body: JSON.stringify(data)
-        })
-        .then((response) => response.json().catch(()=>{}))
-        .then(setData)
-        .catch(setError)
-        .finally(()=> setLoading(false))
-    }
-    return [mutation, {loading, data, error}];
+      .then((response) => response.json().catch(() => {}))
+      .then((data) => setState((prev) => ({ ...prev, data })))
+      .catch((error) => setState((prev) => ({ ...prev, error })))
+      .finally(() => setState((prev) => ({ ...prev, loading: false })));
+  }
+  return [mutation, { ...state }];
 }
--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,10 +1,10 @@
 import type { NextPage } from "next";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import Button from "../components/button";
-import Input from "../components/input";
-import useMutation from "../libs/client/useMutation";
-import { cls } from "../libs/client/utils";
+import Button from "@components/button";
+import Input from "@components/input";
+import useMutation from "@libs/client/useMutation";
+import { cls } from "@libs/client/utils";
 
 interface EnterForm {
   email?: string;
@@ -12,8 +12,8 @@ interface EnterForm {
 }
 
 const Enter: NextPage = (props) => {
-  const [enter, {loading, data, error}] = useMutation("/api/users/enter")
-  const [ submitting, setSubmitting ] = useState(false);
+  const [enter, { loading, data, error }] = useMutation("/api/users/enter");
+  const [submitting, setSubmitting] = useState(false);
   const { register, handleSubmit, reset } = useForm<EnterForm>();
   const [method, setMethod] = useState<"email" | "phone">("email");
   const onEmailClick = () => {
@@ -27,8 +27,6 @@ const Enter: NextPage = (props) => {
   const onValid = (validForm: EnterForm) => {
     enter(validForm);
   };
-console.log(loading, data, error);
-
   return (
     <div className="mt-16 px-4">
       <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
